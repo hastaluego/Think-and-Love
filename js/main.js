282ef4e4a10e7829518f43351bce081f0fa012ef
Rodrigo de Melo <rodrigoeddie@gmail.com>
2015-01-12 15:20:48 -0200

    alteracoes

--- js/main.js
+++ js/main.js
@@ -1,45 +1,37 @@
+var zoom = window.innerWidth/640;
+document.getElementById('resize-content').style.opacity = 1;
+document.getElementById('resize-content').style.zoom = zoom;
+
+function _link(data){
+   window.open(decodeURIComponent(data), '_system');
+}
+
 $(document).ready(function(){
-    header_components_init();
 
-    
-    if ((navigator.userAgent.indexOf('iPhone') != -1) || (navigator.userAgent.indexOf('iPod') != -1)){
-        jQuery('select').selectric({
-            disableOnMobile: false
+    $( "#conteudo" ).load( "http://thinkandlove.com.br/m_/" , function(){
+        $('.loading').hide();
+    });
+
+    $(document).on('touchstart', '.btn', function(e){
+        e.preventDefault();
+
+        $('.loading').show();
+        $( "#conteudo" ).load( $(this).attr('rel') , function(){
+            $('.loading').hide();
         });
-    }
 
-    $('.btn-youtube').on('click', function(e){
-        e.preventDefault();
-        $('.box-iframe').fadeIn();
-        var _this = $(this);
-        var ifr = document.getElementById('loader');
-        var str = _this.attr('href').split("v=");
-        var str = str[1].split("&");
-        ifr.src = '//www.youtube.com/embed/'+str[0];
-        return false; 
+        return false;
     });
 
-    $('.box-iframe .close, .box-iframe ').on('click', function(e){
-        e.preventDefault();
-        $('.box-iframe').fadeOut();
-        var ifr = document.getElementById('loader');
-        ifr.src = '';
-        return false; 
-    });
-
-});
-
-function header_components_init(){
-    var $body       = $('body'),
-        $btnmenu    = $('.header .btn-menu'),
-        $btnmenuclose    = $('.overlay .btn-menu'),
-        $btnvoltar  = $('.btn-voltar'),
-        $overlay    = $('.overlay'),
-        $nav        = $overlay.find('nav'),
-        eventClick  = 'click';
+    var $body         = $('body'),
+        $btnmenu      = $('.header .btn-menu'),
+        $btnmenuclose = $('.overlay .btn-menu'),
+        $btnvoltar    = $('.btn-voltar'),
+        $overlay      = $('.overlay'),
+        $nav          = $overlay.find('nav'),
+        eventClick    = 'touchstart';
 
     $btnmenu.on(eventClick, function(e){
-
         setTimeout(function(){
 
             $overlay.fadeIn(200, function(){
@@ -70,4 +62,31 @@ function header_components_init(){
 
         window.history.back();
     });
-}
+
+    
+    if ((navigator.userAgent.indexOf('iPhone') != -1) || (navigator.userAgent.indexOf('iPod') != -1)){
+        jQuery('select').selectric({
+            disableOnMobile: false
+        });
+    }
+
+    $(document).on('touchstart', '.btn-youtube', function(e){
+        e.preventDefault();
+        $('.box-iframe').fadeIn();
+        var _this = $(this);
+        var ifr = document.getElementById('loader');
+        var str = _this.attr('rel').split("v=");
+        var str = str[1].split("&");
+        ifr.src = '//www.youtube.com/embed/'+str[0];
+        return false; 
+    });
+
+    $(document).on('touchstart', '.box-iframe .close, .box-iframe', function(e){
+        e.preventDefault();
+        $('.box-iframe').fadeOut();
+        var ifr = document.getElementById('loader');
+        ifr.src = '';
+        return false; 
+    });
+
+});
