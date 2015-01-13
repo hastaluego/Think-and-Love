ef88fe628b22220d95a67e45b56d4c14a19a8356
Rodrigo de Melo <rodrigoeddie@gmail.com>
2015-01-13 16:04:51 -0200

    alteracao

--- js/main.js
+++ js/main.js
@@ -30,8 +30,7 @@ $(document).ready(function(){
         $( "#conteudo" ).load( $(this).attr('rel') , function(){
             $('.loading').hide();
 
-            $('.box-iframe').css('height' , $(window).height() / zoom );
-
+            // PAGINA CAUSAS
             if ( $(_button).attr('rel').indexOf("causas") != -1 ) {
                 $.each( $('.causas article') , function(i, el){
                     var _this = this;
@@ -44,6 +43,7 @@ $(document).ready(function(){
                 });
             };
 
+            // PAGINA ONGS
             if ( $(_button).attr('rel').indexOf("ongs") != -1 || $(_button).attr('rel').indexOf("causas_categoria") != -1 ) {
                 $.each( $('.ong-list article img') , function(){
                     var _this = this;
@@ -58,13 +58,15 @@ $(document).ready(function(){
                 }
             };
 
+            // PAGINA CAUSA
             if ( $(_button).attr('rel').indexOf("causa") != -1 ) {
                 $.each( $('.box-img img') , function(){
                     var _this = this;
-
                     $(_this).attr('src' , $(_this).attr('src').replace("/m/", '') );
                 });
             };
+
+            $('.box-iframe').css('height' , $(window).height() / zoom );
         });
 
         return false;
